2ae6281093f43b7826971153268bea111ccc512f
ninapgarcia <ninapgarcia@gmail.com>
2022-03-02 10:58:27 -0300

    inicial commit

--- cipher.py
+++ cipher.py
@@ -10,5 +10,8 @@ message, key = utils.menu()
 cipher_message = utils.cipherShift(message, key)
 print("MENSAGEM CIFRADA: ", cipher_message)
 
+print("\n------------ DECRYPTOR -------------\n")
+
+
 decrypted_message = utils.decryptorShift(cipher_message, key)
 print("MENSAGEM DECIFRADA: ", decrypted_message)
